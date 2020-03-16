be36cd629e8a86e530656093751228a95ed1cca1
wuxihui <wuxihui@hzhfzx.com>
2020-03-16 16:50:04 +0800

    测试

--- src/pages/System.vue
+++ src/pages/System.vue
@@ -1,11 +1,38 @@
 <template>
   <div class="el-system">
       system
+      <div class="box">
+        <ul>
+          <li @click="handleClick">dfgfsdg</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+          <li>页面内容</li>
+        </ul>
+      </div>
   </div>
 </template>
 
 <script>
  export default {
-     name: "system"
+     name: "system",
+     methods: {
+       handleClick(event) {
+          let el = event.currentTarget;
+          console.log(el);
+          
+       }
+     }
  }
 </script>
+
+<style scoped lang="less">
+
+</style>
