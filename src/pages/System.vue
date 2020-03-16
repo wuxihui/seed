a0208bb256049f72c97dc54b8dd3b4db841518d4
wuxihui <wuxihui@hzhfzx.com>
2020-03-16 19:26:38 +0800

    设置scrollTop

--- src/pages/System.vue
+++ src/pages/System.vue
@@ -2,8 +2,8 @@
   <div class="el-system">
       system
       <div class="box">
-        <ul>
-          <li @click="handleClick">dfgfsdg</li>
+        <ul id="mostUl">
+          <li>我是需要的div</li>
           <li>页面内容</li>
           <li>页面内容</li>
           <li>页面内容</li>
@@ -17,6 +17,7 @@
           <li>页面内容</li>
         </ul>
       </div>
+      <div class="search" @click="handleSearch">搜索</div>
   </div>
 </template>
 
@@ -24,15 +25,34 @@
  export default {
      name: "system",
      methods: {
-       handleClick(event) {
-          let el = event.currentTarget;
-          console.log(el);
-          
-       }
+      handleSearch() {
+        let li = document.getElementById("mostUl").getElementsByTagName("li");
+        console.log(li);
+        for(let i=0; i<li.length; i++) {
+            let itemi = li[i];
+            let content = itemi.innerHTML;
+            let data = "我是需要的div";
+            if(content.trim() == data.trim()) {
+                console.log(888);
+                console.log(itemi);
+                let list = document.getElementById("mostUl").getElementsByTagName('li')[i].offsetTop;
+                let targetUl = document.querySelector(".box");
+                console.log(targetUl.scrollTop);
+                targetUl.scrollTop = list - 15;
+                console.log(list);
+                console.log(targetUl.scrollTop);
+                
+            }
+        }
+       
+      }
      }
  }
 </script>
 
 <style scoped lang="less">
-
+.box {
+  height: 100px;
+  overflow-y: auto;
+}
 </style>
