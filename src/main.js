7ab6722e5a3857413aeb7f3c30d6e20d33bd4a44
wuxihui <wuxihui@hzhfzx.com>
2020-04-07 18:19:15 +0800

    div拉升

--- src/main.js
+++ src/main.js
@@ -7,7 +7,11 @@ import router from './router'
 import store from './store'
 import commonJS from './common/js'
 import _ from "lodash"
+//
+import VueDraggableResizable from 'vue-draggable-resizable'
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 
+Vue.component('vue-draggable-resizable', VueDraggableResizable)
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
