d95f5f10d80b8686e5b07c4945594eb9ae2e39ad
wuxihui <2263850089@qq.com>
2020-04-07 21:26:40 +0800

    拉升

--- src/pages/plate/PlateList.vue
+++ src/pages/plate/PlateList.vue
@@ -3,17 +3,22 @@
         <button @click="handleClick">消息提示</button>
         <button @click="handleTimeClick">获取后台时间格式</button>
         <p>{{formatTime(time)}}</p>
-                <!-- @mousedown="mouseDownHandleelse($event)" 
-                @mouseup="mouseUpHandleelse($event)"> -->
+
+        <!-- <div 
+          class="box" 
+          id="box" 
+          @mousedown="down"
+          ></div> -->
+
         <div class="form-all-style">
             <div style="height: 432px" id="zhankaiStyle" class="bottom-form" ref="kongtiao"
                @mousedown="mouseDownHandleelse($event)" 
-               @mouseup="mouseUpHandleelse($event)">
-               >
+               @mouseleave="leave($event)"
+               @mouseup="mouseUpHandleelse()">
             </div>
         </div>
         
-
+       
 
     </div>
 </template>
@@ -47,18 +52,18 @@ export default {
     },
     watch: {
       // 定位top的变化值，就是元素高度的变化值，通过原始值和元素top定位的变化值最终得到的就是height高度值
-    //   TopDongChange(newVal, oldVal) {
-    //     var reduce
-    //     // 因为oldVal起始值为null，所以先排除掉
-    //     if (!oldVal && typeof oldVal === 'object') {
-    //       console.log(oldVal);
-    //       reduce = 0
-    //     } else {
-    //       reduce = oldVal - newVal
-    //     }
-    //     document.getElementById('zhankaiStyle').style.height = Number(Cookies.get('chazhiValue')) + reduce + 'px'
-    //     Cookies.set('chazhiValue', Number(Cookies.get('chazhiValue')) + reduce)
-    //   }
+      TopDongChange(newVal, oldVal) {
+        var reduce
+        // 因为oldVal起始值为null，所以先排除掉
+        if (!oldVal && typeof oldVal === 'object') {
+          console.log(oldVal);
+          reduce = 0
+        } else {
+          reduce = oldVal - newVal
+        }
+        document.getElementById('zhankaiStyle').style.height = Number(Cookies.get('chazhiValue')) + reduce + 'px'
+        Cookies.set('chazhiValue', Number(Cookies.get('chazhiValue')) + reduce)
+      }
     },
     methods: {
         handleClick() {
@@ -77,39 +82,80 @@ export default {
 
 
         //鼠标按下
-      mouseDownHandleelse(event) {
-        this.moveDataelse.y = event.pageY - this.$refs.kongtiao.offsetTop;
-        event.currentTarget.style.cursor = 'move';
+      mouseDownHandleelse(e) {
+          e=e || window.event;
+        // this.pauseEvent(e);
+        this.moveDataelse.y = e.pageY - this.$refs.kongtiao.offsetTop;
+        e.currentTarget.style.cursor = 'move';
         window.onmousemove = this.mouseMoveHandleelse;
         event.preventDefault();
       },
        // 鼠标移动
       mouseMoveHandleelse(event) {
         this.topHeight = event.pageY - this.moveDataelse.y;
-        // this.topHeight = this.topHeight < this.minHeight ? this.minHeight : this.topHeight;
-        // this.topHeight = this.topHeight > this.maxHeight ? this.maxHeight : this.topHeight;
-        // this.moveDataelse.TopDong = this.topHeight;
+        this.topHeight = this.topHeight < this.minHeight ? this.minHeight : this.topHeight;
+        this.topHeight = this.topHeight > this.maxHeight ? this.maxHeight : this.topHeight;
+        this.moveDataelse.TopDong = this.topHeight;
         console.log(666);
         console.log("this.maxHeight:", this.maxHeight);
         console.log("this.minHeight:", this.minHeight);
         console.log("this.moveDataelse.topMove:", this.moveDataelse.topMove);
         console.log("this.topHeight:", this.topHeight);
-        if(this.topHeight <= this.minHeight) {
-            window.onmousemove = null;
-        } else if(this.topHeight >= this.maxHeight) {
-            window.onmousemove = null;
-        } else {
-            this.moveDataelse.TopDong = this.topHeight;
-        }
         //区别onclick事件
         return false;
       },
        // 鼠标松开
-      mouseUpHandleelse(event) {
-        console.log(event);
+      mouseUpHandleelse(e) {
+        console.log(8888);
         window.onmousemove = null;
-        event.currentTarget.style.cursor = 'move';
+        e.currentTarget.style.cursor = 'move';
       },
+      pauseEvent(e){
+            if(e.stopPropagation) e.stopPropagation();
+            if(e.preventDefault) e.preventDefault();
+            e.cancelBubble=true;
+            e.returnValue=false;
+            return false;
+        },
+
+     leave(e) {
+         this.mouseUpHandleelse(e);
+     },
+     down: function(e){
+        e = e ||event;
+        var oBox = document.getElementById("box");
+        var y = e.clientY;
+        var oBoxH = oBox.offsetHeight;
+        document.onmousemove = function(e){
+            var yy = e.clientY;
+            let topHeight = oBoxH + yy - y + 'px';
+            console.log(topHeight);
+            // topHeight = topHeight < 200 ? 200 : topHeight;
+            // topHeight = topHeight > 600 ? 600 : topHeight;
+            console.log(888);
+            
+            console.log(oBox.style.height);
+            
+            oBox.style.height = topHeight;
+            if(oBox.style.height == 200 + "px") {
+                oBox.style.height = 200;
+                console.log(666);
+                console.log(oBox.style.height);
+            } else if(oBox.style.height == 600 + "px") {
+                oBox.style.height = 600;
+            } else {
+                oBox.style.height = topHeight;
+            }
+            return false;
+        }
+        document.onmouseup = function(){
+            document.onmousemove = null;
+            document.onmouseup = null;
+        }
+        if(e.preventDefault){
+            e.preventDefault();
+        }
+    }
     }
 }
 </script>
@@ -128,5 +174,12 @@ export default {
     height: 432px;
     padding-top: 30px;
     background-color: saddlebrown;
+}
+.box {
+    width: 100%;
+    height: 300px;
+    position: absolute;
+    background: red;
+    margin-top: 20px;
 }
 </style>
